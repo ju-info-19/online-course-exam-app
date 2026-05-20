0636364ee0b8b7a40b2703cc8f287a6fed7d9f22
ju-info-19 <158595910+ju-info-19@users.noreply.github.com>
2026-05-20 21:51:21 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,8 +6,7 @@ app_name = 'onlinecourse'
 urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('course/<int:course_id>/', views.course_detail, name='course_detail'),
-    path('lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
-    path('exam/<int:lesson_id>/', views.take_exam, name='take_exam'),
-    path('exam/<int:lesson_id>/submit/', views.submit_exam, name='submit_exam'),  # Chemin pour submit
-    path('exam/<int:lesson_id>/result/', views.show_exam_result, name='show_exam_result'),  # Chemin pour show_exam_result
+    path('course/<int:course_id>/exam/', views.take_exam, name='take_exam'),
+    path('course/<int:course_id>/submit/', views.submit_exam, name='submit_exam'),
+    path('course/<int:course_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
